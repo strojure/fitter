69e11fdcd84c009c5feb4d6a7ffa0c8972700898
Sergey Trofimov <sergey@trofimov.name>
2022-05-09 10:27:49 +0200

    Add leiningen project

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject strojure/fitter "0.1.0-SNAPSHOT"
+  :description "Dependency injection for Clojure"
+  :url "https://github.com/strojure/fitter"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+
+  :dependencies [[org.clojure/clojure "1.11.1" :scope "provided"]]
+
+  :profiles {:dev {:dependencies []}})
