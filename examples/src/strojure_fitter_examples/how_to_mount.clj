91a0b5151db4d123d608b5c161529ef6466720f1
Sergey Trofimov <sergey@trofimov.name>
2022-05-14 17:10:08 +0200

    Add suspend/resume for mounting components

--- examples/src/strojure_fitter_examples/how_to_mount.clj
+++ examples/src/strojure_fitter_examples/how_to_mount.clj
@@ -33,11 +33,27 @@
   []
   {:test-deps :default})
 
+(def ^{::component/start (fn [system]
+                           (println "Start" 'test-suspend)
+                           {:test-suspend (:system/x system)})
+       ::component/stop! (fn [inst]
+                           (println "Stop" 'test-suspend inst))
+       ::component/suspend! (fn [inst _old-system]
+                              (println "Suspend" 'test-suspend inst)
+                              (fn [system]
+                                (println "Resume" 'test-suspend)
+                                {:test-suspend (:system/x system) :resumed true}))}
+  test-suspend
+  "The value which can suspend. It is unmounted when suspended."
+  {:test-suspend :default})
+
 (declare ^{:doc "Declared but not initialized value which cannot be used without system start."
            ::component/start (fn [system]
                                (println "Start" 'test-declare)
                                (constantly {:test-declare (:system/x system)}))}
          test-declare)
+
+
 
 ;;••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
 
@@ -46,28 +62,45 @@
    `test-function (mount/component `test-function)
    `test-deps (mount/component `test-deps)
    `test-value (mount/component `test-value)
+   `test-suspend (mount/component `test-suspend)
    `test-declare (mount/component `test-declare)})
 
 ;;••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
 
 (defn- print-status
   []
-  (println "Status -" (test-function) test-value (test-deps)
+  (println "Status -" (test-function) test-value test-suspend (test-deps)
            (try (test-declare) (catch Throwable e (ex-message e)))))
 
 (comment
   (do (print-status)
       (with-open [system! (system/init)]
         (system/start! system! {:registry registry})
+        (print-status)
+        (system/stop! system! {:suspend true})
+        (print-status)
+        (system/start! system!)
         (print-status))
       (print-status))
-  ;Status - {:test-function :default} {:test-value :default} Attempting to call unbound fn: #'examples.strojure-fitter.mount/test-declare
+  ;Status - {:test-function :default} {:test-value :default} {:test-suspend :default} {:test-deps :default} Attempting to call unbound fn: #'strojure-fitter-examples.how-to-mount/test-declare
   ;Start test-function
+  ;Start test-deps
   ;Start test-value
+  ;Start test-suspend
   ;Start test-declare
-  ;Status - {:test-function :x} {:test-value :x} {:test-declare :x}
+  ;Status - {:test-function :x} {:test-value :x} {:test-suspend :x} {:test-deps {:test-value :x}} {:test-declare :x}
   ;Stop test-value {:test-value :x}
-  ;Status - {:test-function :default} {:test-value :default} Attempting to call unbound fn: #'examples.strojure-fitter.mount/test-declare
+  ;Suspend test-suspend {:test-suspend :x}
+  ;Status - {:test-function :default} {:test-value :default} {:test-suspend :default} {:test-deps :default} Attempting to call unbound fn: #'strojure-fitter-examples.how-to-mount/test-declare
+  ;Start test-function
+  ;Start test-deps
+  ;Start test-value
+  ;Resume test-suspend
+  ;Start test-declare
+  ;Status - {:test-function :x} {:test-value :x} {:test-suspend :x, :resumed true} {:test-deps {:test-value :x}} {:test-declare :x}
+  ;Stop test-value {:test-value :x}
+  ;Stop test-suspend {:test-suspend :x, :resumed true}
+  ;Status - {:test-function :default} {:test-value :default} {:test-suspend :default} {:test-deps :default} Attempting to call unbound fn: #'strojure-fitter-examples.how-to-mount/test-declare
   )
 
 ;;••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
--- src/strojure/fitter/mount.clj
+++ src/strojure/fitter/mount.clj
@@ -11,21 +11,24 @@
   (or (resolve sym)
       (throw (ex-info (str "Unresolved var for symbol: " sym) {:type ::error}))))
 
-(defn- mount-var
-  [v, new-val]
+(defn- mount-instance
+  "Mounts instance to var, returns nil."
+  [instance, v]
   ;; TODO: mount over mounted - should we care?
   (let [original (or (::original (meta v))
                      (deref v))]
     (doto v
-      (alter-var-root (constantly new-val))
-      (alter-meta! assoc ::original original))))
+      (alter-var-root (constantly instance))
+      (alter-meta! assoc ::original original)))
+  nil)
 
 (defn- restore-var
   [v]
   (when-let [original (-> v meta ::original)]
     (doto v
       (alter-var-root (constantly original))
-      (alter-meta! dissoc ::original))))
+      (alter-meta! dissoc ::original)))
+  nil)
 
 ;;••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
 
@@ -33,21 +36,31 @@
   "Returns component mounting started instance in existing var using var’s meta
   as component."
   [sym]
-  {::component/start (fn start [system]
-                       (let [v (resolve-sym sym)
-                             start (::component/start (meta v))
-                             _ (when-not start
-                                 (throw (ex-info (str "Symbol is not a component: " sym)
-                                                 {:type ::error :meta (meta v)})))
-                             instance (start system)]
-                         (mount-var v instance)
-                         instance))
-   ::component/stop! (fn stop! [instance]
-                       (let [v (resolve-sym sym)]
-                         (try
-                           (when-let [stop (::component/stop! (meta v))]
-                             (stop instance))
-                           (finally
-                             (restore-var v)))))})
+  (reify component/Component
+    (start [_ system]
+      (let [v (resolve-sym sym)
+            start-fn (::component/start (meta v))]
+        (when-not start-fn
+          (throw (ex-info (str "Symbol is not a component: " sym)
+                          {:type ::error :meta (meta v)})))
+        (doto (start-fn system)
+          (mount-instance v))))
+
+    (stop-fn [_]
+      (let [v (resolve-sym sym)
+            stop-fn (::component/stop! (meta v))]
+        (fn [instance]
+          (restore-var v)
+          (when stop-fn (stop-fn instance)))))
+
+    (suspend-fn [_]
+      (let [v (resolve-sym sym)]
+        (when-let [suspend-fn (::component/suspend! (meta v))]
+          (fn suspend! [instance old-system]
+            (when-let [resume-fn (suspend-fn instance old-system)]
+              (restore-var v)
+              (fn resume [new-system]
+                (doto (resume-fn new-system)
+                  (mount-instance v))))))))))
 
 ;;••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
