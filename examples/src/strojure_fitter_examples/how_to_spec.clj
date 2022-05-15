6380bb5d13384377ada73bb0606db4645b1c7fb5
Sergey Trofimov <sergey@trofimov.name>
2022-05-15 12:18:21 +0200

    Add how-to-spec, fix missing file

---
+++ examples/src/strojure_fitter_examples/how_to_spec.clj
@@ -0,0 +1,42 @@
+(ns strojure-fitter-examples.how-to-spec
+  "Example how to spec component arguments on system start."
+  (:require [clojure.spec.alpha :as s]
+            [strojure.fitter.component :as component]
+            [strojure.fitter.system :as system]))
+
+(set! *warn-on-reflection* true)
+
+;;••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
+
+(s/def ::a int?)
+(s/def ::b string?)
+
+(defmulti ^:private component-spec identity)
+(defmethod component-spec :default [_] nil)
+(defmethod component-spec ::a [_] (s/keys :req [::b]))
+
+(defn- wrap-spec
+  [[k c]]
+  [k (component/bundle (fn spec-start [system]
+                         (some-> (component-spec k) (s/assert* system))
+                         (component/start c system))
+                       (component/stop-fn c)
+                       (component/suspend-fn c))])
+
+(def ^:private registry
+  (->> {::a (comp parse-long ::b)
+        ::b (constantly "1")}
+       (into {} (map wrap-spec))))
+
+;;••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
+
+(def ^:private system!
+  (system/init {:registry registry}))
+
+(comment
+  (system/start! system!)
+  (system/start! system! {:registry (dissoc registry ::b)})
+  (system/start! system! {:registry (assoc registry ::b (constantly :k))})
+  )
+
+;;••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
