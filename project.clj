f7b34cec34dd11650bb465231da620b936447725
Sergey Trofimov <sergey@trofimov.name>
2022-05-23 12:21:29 +0200

    Bump snapshot version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.strojure/fitter "0.5.58-beta"
+(defproject com.github.strojure/fitter "0.6.59-SNAPSHOT"
   :description "System component management library for Clojure"
   :url "https://github.com/strojure/fitter"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
