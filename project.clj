80d4190270ae9e0295703e49931fe3a7b03e2dd5
Sergey Trofimov <sergey@trofimov.name>
2022-06-01 11:59:57 +0200

    Release version `1.0.68`

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.strojure/fitter "0.8.66-SNAPSHOT"
+(defproject com.github.strojure/fitter "1.0.68"
   :description "System component management library for Clojure"
   :url "https://github.com/strojure/fitter"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
