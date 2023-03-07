a97c5b50e4f69b4b512f6f90adf18f52b50a5373
Sergey Trofimov <sergey@trofimov.name>
2023-03-07 17:44:43 +0100

    (chore) Change license to MIT

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,7 +8,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
 Release date `UNRELEASED`
 
-
+- (chore) Change license to MIT.
 
 ## `1.1.0-73`
 
--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject com.github.strojure/fitter "1.1.1-74-SNAPSHOT"
   :description "System component management library for Clojure"
   :url "https://github.com/strojure/fitter"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "The MIT License" :url "http://opensource.org/licenses/MIT"}
 
-  :dependencies [[org.clojure/clojure "1.11.1" :scope "provided"]]
+  :dependencies []
 
-  :profiles {:dev {:source-paths ["examples/src"]
+  :profiles {:provided {:dependencies [[org.clojure/clojure "1.11.1"]]}
+             :dev {:source-paths ["examples/src"]
                    :dependencies []}})
