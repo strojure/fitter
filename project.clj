78264fc5a30d5b4b6733ce894d413d186d97aca2
Sergey Trofimov <sergey@trofimov.name>
2023-03-08 11:18:08 +0100

    (chore) Bump snapshot version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.strojure/fitter "1.1.1-82"
+(defproject com.github.strojure/fitter "1.1.2-83-SNAPSHOT"
   :description "System component management library for Clojure"
   :url "https://github.com/strojure/fitter"
   :license {:name "The MIT License" :url "http://opensource.org/licenses/MIT"}
